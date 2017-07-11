734fcdba984e27454668704ae7a0743b40d17cf2
Alexey Korol <korol@devexperts.com>
2017-07-11 18:49:16 +0300

    Workarea update

--- src/components/WorkArea/WorkArea.jsx
+++ src/components/WorkArea/WorkArea.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import css from './WorkArea.styl';
 import butotnCss from '../Button/Button.styl';
 import Button from '../Button/Button';
+import ButtonIcon from '../ButtonIcon/ButtonIcon';
 import ToggleButtons from 'dx-components/src/components/ToggleButtons/ToggleButtons';
-const toggleTheme = {
+import Widget from '../Widget/Widget';
+import InstrumentPane from '../InstrumentPane/InstrumentPane';
+
+import {themeable} from 'react-css-themr';
+import toggleButtonsCss from './theme/ToggleButtons.styl';
+
+const toggleButtonsTheme = themeable(toggleButtonsCss, {
 	container__item_active: butotnCss.buttonActive
-};
+});
 
 export default class WorkArea extends React.Component {
 
@@ -17,17 +24,32 @@ export default class WorkArea extends React.Component {
 		return (
 			<div className={css.container}>
 				<div className={css.header}>
-					<ToggleButtons toggleIndex={this.state.toggleIndex}
-								   theme={toggleTheme}
-								   onChange={this.onToggleChange}>
-						<Button>Основа</Button>
-						<Button>Дизайн</Button>
-						<Button>Текст</Button>
-						<Button>Загрузить</Button>
-					</ToggleButtons>
+					<div className={css.mode}>
+						<ToggleButtons toggleIndex={this.state.toggleIndex}
+									   theme={toggleButtonsTheme}
+									   onChange={this.onToggleChange}>
+							<Button>Основа</Button>
+							<Button>Дизайн</Button>
+							<Button>Текст</Button>
+							<Button>Загрузить</Button>
+						</ToggleButtons>
+					</div>
+					<ButtonIcon name='zoom-in' />
+					<ButtonIcon name='zoom-out' />
+					<ButtonIcon name='delete' />
+					<ButtonIcon name='fullscreen' />
 				</div>
 				<div className={css.content}>
-					
+					<div className={css.aside}>
+					</div>
+					<div className={css.stage}>
+						<Widget header={<div>Выбор основы</div>}>
+							test
+						</Widget>
+					</div>
+					<div className={css.aside}>
+						<InstrumentPane />
+					</div>
 				</div>
 			</div>
 		)
